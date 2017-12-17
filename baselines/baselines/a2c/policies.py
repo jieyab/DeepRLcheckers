59655011bad51799e00f9328cf014425cc895b84
Khmer <adrianalvarez15@gmail.com>
2017-12-17 15:57:13 +0100

    Small changes in RL agent

--- baselines/baselines/a2c/policies.py
+++ baselines/baselines/a2c/policies.py
@@ -15,11 +15,11 @@ class LnLstmPolicy(object):
         M = tf.placeholder(tf.float32, [nbatch]) #mask (done t-1)
         S = tf.placeholder(tf.float32, [nenv, nlstm*2]) #states
         with tf.variable_scope("model", reuse=reuse):
-            h = conv(tf.cast(X, tf.float32) / 255., 'c1', nf=8, rf=2, stride=1, init_scale=np.sqrt(2))
-            h2 = conv(h, 'c2', nf=16, rf=2, stride=1, init_scale=np.sqrt(2))
-            # h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
+            h = conv(tf.cast(X, tf.float32), 'c1', nf=16, rf=2, stride=1, init_scale=np.sqrt(2))
+            h2 = conv(h, 'c2', nf=32, rf=2, stride=1, init_scale=np.sqrt(2))
+            h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
             h3 = conv_to_fc(h2)
-            h4 = fc(h3, 'fc1', nh=128, init_scale=np.sqrt(2))
+            h4 = fc(h3, 'fc1', nh=32, init_scale=np.sqrt(2))
             xs = batch_to_seq(h4, nenv, nsteps)
             ms = batch_to_seq(M, nenv, nsteps)
             h5, snew = lnlstm(xs, ms, S, 'lstm1', nh=nlstm)
@@ -28,7 +28,7 @@ class LnLstmPolicy(object):
             vf = fc(h5, 'v', 1, act=lambda x:x)
 
         v0 = vf[:, 0]
-        a0 = sample(pi)
+        a0 = sample_without_exploration(pi)
         self.initial_state = np.zeros((nenv, nlstm*2), dtype=np.float32)
 
         def step(ob, state, mask):
@@ -53,15 +53,15 @@ class LstmPolicy(object):
         nh, nw, nc = ob_space.shape
         ob_shape = (nbatch, nh, nw, nc*nstack)
         nact = 9 #ac_space.n
-        X = tf.placeholder(tf.uint8, ob_shape) #obs
+        X = tf.placeholder(tf.float32, ob_shape) #obs
         M = tf.placeholder(tf.float32, [nbatch]) #mask (done t-1)
         S = tf.placeholder(tf.float32, [nenv, nlstm*2]) #states
         with tf.variable_scope("model", reuse=reuse):
-            h = conv(tf.cast(X, tf.float32)/255., 'c1', nf=8, rf=2, stride=1, init_scale=np.sqrt(2))
-            h2 = conv(h, 'c2', nf=16, rf=2, stride=1, init_scale=np.sqrt(2))
-            #h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
-            h3 = conv_to_fc(h2)
-            h4 = fc(h3, 'fc1', nh=128, init_scale=np.sqrt(2))
+            h = conv(tf.cast(X, tf.float32), 'c1', nf=32, rf=2, stride=1, init_scale=np.sqrt(2))
+            h2 = conv(h, 'c2', nf=64, rf=2, stride=1, init_scale=np.sqrt(2))
+            h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
+            h3 = conv_to_fc(h3)
+            h4 = fc(h3, 'fc1', nh=32, init_scale=np.sqrt(2))
             xs = batch_to_seq(h4, nenv, nsteps)
             ms = batch_to_seq(M, nenv, nsteps)
             h5, snew = lstm(xs, ms, S, 'lstm1', nh=nlstm)
--- baselines/baselines/a2c/run_gomoku.py
+++ baselines/baselines/a2c/run_gomoku.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
-import os, logging, gym
-from baselines import logger
-from baselines.common import set_global_seeds
-from baselines import bench
 from baselines.a2c.a2c import learn
-from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
-from baselines.common.atari_wrappers import wrap_deepmind
 from baselines.a2c.policies import CnnPolicy, LstmPolicy, LnLstmPolicy
 from AlphaGomoku.games.tic_tac_toe_x import TicTacToeXGameSpec
 
 if __name__ == '__main__':
 
-    policy_fn = LnLstmPolicy
+    policy_fn = CnnPolicy
     env = TicTacToeXGameSpec(3, 3)
     learn(policy_fn, env, nsteps=10
           ,nstack=1,  seed= 0,total_timesteps=10000000,load_model=False, model_path='./models/tic_tac_toe.cpkt')
