a0360f326347e60112ac7d300247ece9d352c1e2
Khmer <adrianalvarez15@gmail.com>
2018-03-06 02:03:37 +0100

    Some changes

--- AlphaGomoku/core/run_gomoku_2.py
+++ AlphaGomoku/core/run_gomoku_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from AlphaGomoku.core.a2c_random import learn
-from AlphaGomoku.core.policies import CnnPolicy_slim_scope5x5, CnnPolicy_slim_scope5x5_1x1,CnnPolicy_slim_scope9x9_1x1
+from AlphaGomoku.core.a2c_2 import learn
+from AlphaGomoku.core.policies import CnnPolicy_slim_scope5x5, CnnPolicy_slim_scope5x5_1x1, CnnPolicy_slim_scope5x5_3, \
+    CnnPolicy_slim_scope5x5_3_1x1
 from AlphaGomoku.games.tic_tac_toe_x_2 import TicTacToeXGameSpec
 
 if __name__ == '__main__':
@@ -11,8 +12,8 @@ if __name__ == '__main__':
 
     seed = 5
 
-    BATCH_SIZE = 64
-    TEMP_CTE = 30000
+    BATCH_SIZE = 128
+    TEMP_CTE = 5000
 
     data_augmentation = True
 
@@ -23,4 +24,4 @@ if __name__ == '__main__':
     env = TicTacToeXGameSpec(size_board, winning_length)
     learn(policy_fn, env, nsteps=size_board * size_board, nstack=1, seed=seed, total_timesteps=10000000,
           load_model=False, model_path=model_path, data_augmentation=data_augmentation,
-          BATCH_SIZE=BATCH_SIZE, TEMP_CTE=TEMP_CTE)
+          BATCH_SIZE=BATCH_SIZE, TEMP_CTE=TEMP_CTE, expert=True)
