3b4980de6ab8b3bb86dc6bd57d64bcdbee0df1e3
Khmer <adrianalvarez15@gmail.com>
2018-03-08 20:29:05 +0100

    Fix sh files

--- AlphaGomoku/core/run_expert_train.sh
+++ AlphaGomoku/core/run_expert_train.sh
@@ -7,4 +7,4 @@
 #SBATCH --mem=2GB#(This is max available RAM)
 module load Python/3.5.2-foss-2016a
 module load tensorflow/1.2.0-foss-2016a-Python-3.5.2
-python3 run_gomoku_VS_terminal.py $*
+python3 run_gomoku_2_terminal.py $*
